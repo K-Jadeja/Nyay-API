22551f3ea3daa8d35086954b061b4b2ca8a1f420
Krishnasinh Jadeja <itsk.jadeja@gmail.com>
2023-09-21 22:29:43 +0530

    update pinecone.py

--- pinecone.py
+++ pinecone.py
@@ -1,11 +1,11 @@
-import pinecone
-from langchain.vectorstores import Pinecone
+# import pinecone
+# from langchain.vectorstores import Pinecone
 
-pinecone.init(
-    api_key="f7aa89af-ac99-4619-8914-8d08740f7b38",  # find at app.pinecone.io
-    environment="us-east-1-aws"  # next to api key in console
-)
+# pinecone.init(
+#     api_key="f7aa89af-ac99-4619-8914-8d08740f7b38",  # find at app.pinecone.io
+#     environment="us-east-1-aws"  # next to api key in console
+# )
 
-index_name = "langchain-demo"
+# index_name = "langchain-demo"
 
-# index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+# # index = Pinecone.from_documents(docs, embeddings, index_name=index_name)
